aa2ddb81a8af3a30dd716f08d9f82975da0c4bda
Mohammad <mmh.ksx.2002@gmail.com>
2023-08-06 19:18:17 +0330

    Clean Loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,6 +1,5 @@
 export default function Loader() {
     return (
-
         <div className='fixed inset-0 bg-black/50 z-[100] flex justify-center items-center'>
             <div className='bg-white/80 animate-bounce p-8 main-content-box rounded-3xl'>
                 <div className="spinner">
