482468f4443f038a82648b67c4c7d00c6251171c
Mohammad <mmh.ksx.2002@gmail.com>
2023-08-06 19:26:52 +0330

    Clean Code

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import CommentsProvider from './contexts/CommentsContext'
 import Routes from './Routes'
 import Header from './components/Header'
 import Footer from './components/Footer'
+
 import { ToastContainer } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
 
